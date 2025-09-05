7f47482915ba1f8ed488d1e878b51e9ccf660068
Ashish R <ashrahurkar@yahoo.in>
2025-09-05 15:24:52 -0500

    Integration Test

---
+++ src/test/java/com/example/JavaHelloWorldIT.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldIT {
+
+    @Test
+    void dummyIntegrationTest() {
+        System.out.println("my IT Test");
+    }
+}
